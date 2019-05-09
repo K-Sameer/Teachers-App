e30f3424e993b03ce17dd5ca22766404f8b1f62f
Samir Kak <samir.kak@dataart.com>
2019-05-09 17:07:24 +0300

    Added ugly state and selects.

--- src/components/ParamsForm/ParamsForm.js
+++ src/components/ParamsForm/ParamsForm.js
@@ -1,29 +1,66 @@
 import React, { Component } from 'react'
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+
 
 export default class ParamsForm extends Component {
     constructor(props) {
       super(props)
     
       this.state = {
-         
+        params: [].concat(...this.props.config.groups.map(group => group.controls.map((control) => ({ name:control.name, option:'', value:0 }) )))
       }
+      this.renderSelect = this.renderSelect.bind(this)
     }
+
+    handleChange = event => {
+        console.log('event.target:', event.target);
+        this.setState({ [event.target.name]: event.target.value });
+    };
 
     renderGroup(group){
-        console.log(`Rendering Group: ${group.name}`);
-        group.controls.map((control) => this.renderControl(control))
+        console.log(`RENDERING GROUP: ${group.name}`);
+        return group.controls.map((control) => this.renderControl(control))
     }
 
-    renderControl(control){
-        console.log(`Rendering Control: ${control.name}`);
+    renderControl(control){ 
+        switch (control.type) {
+            case 'select':
+                return this.renderSelect(control);    
+            default:
+                return null;
+        }
     }
 
-    render() {
+    renderSelect(control){
+        return <FormControl>
+        <InputLabel htmlFor={control.name}>{control.label}</InputLabel>
+        <Select
+            onChange={this.handleChange}
+            inputProps={{
+                name: control.name,
+                id: control.name
+              }}
+        >
+            {control.options.map(
+                (option) => (<MenuItem value={option.value} key={option.value}>{option.label}</MenuItem>)
+                )
+            }
+        </Select>
+    </FormControl>
+    }
+
+    render() {       
         return (
         <div>
             <header>ParamsForm</header>
-            {this.props.config.groups.map((group) => this.renderGroup(group))}
+            <form>                
+                {this.props.config.groups.map((group) => this.renderGroup(group))}
+            </form>
         </div>
         )
     }
 }
+
